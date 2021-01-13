72a3c600f5a30822490b6fcc321856680dd39a17
SeanNaren <sean@grid.ai>
2021-01-13 15:31:14 +0000

    Add Carlos idea

--- lightning_transformers/core/huggingface/data.py
+++ lightning_transformers/core/huggingface/data.py
@@ -1,18 +1,14 @@
-from typing import Any, Dict, Optional
+from typing import Any, Dict
 
 from datasets import Dataset, load_dataset
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
-from transformers import PreTrainedTokenizerBase
 
 from lightning_transformers.core import TransformerDataModule
 from lightning_transformers.core.huggingface.config import HFTransformerDataConfig
 
 
 class HFTransformerDataModule(TransformerDataModule):
-    def __init__(self, cfg: HFTransformerDataConfig, tokenizer: Optional[PreTrainedTokenizerBase] = None):
-        super().__init__(cfg)
-        self.cfg = cfg
-        self.tokenizer = tokenizer
+    cfg: HFTransformerDataConfig
 
     def load_dataset(self) -> Dataset:
         if self.cfg.dataset_name is not None:
