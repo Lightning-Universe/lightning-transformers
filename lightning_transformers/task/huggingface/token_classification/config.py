a6976f7c69211c0d627c7997f2b33afa7ad0ebbb
Sean Naren <sean.narenthiran@gmail.com>
2021-01-18 19:08:49 +0000

    Implement Token classification Task (#48)
    * Add base token classification task
    
    * Add metrics, fix data processing
    
    * Address feedback points
    
    * Address feedback

---
+++ lightning_transformers/task/huggingface/token_classification/config.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+from lightning_transformers.core.huggingface.config import HFTransformerDataConfig
+
+
+@dataclass
+class TokenClassificationDataConfig(HFTransformerDataConfig):
+    task_name: str = "ner"
+    label_all_tokens: bool = False
