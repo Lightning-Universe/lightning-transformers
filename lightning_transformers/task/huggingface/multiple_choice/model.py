97f679987456b25d6372072a541cc569297dd439
SeanNaren <sean@grid.ai>
2021-01-20 11:26:23 +0000

    Refactors and added race dataset

--- lightning_transformers/task/huggingface/multiple_choice/model.py
+++ lightning_transformers/task/huggingface/multiple_choice/model.py
@@ -5,7 +5,6 @@ from lightning_transformers.core.huggingface import HFTransformer
 
 
 class MultipleChoiceTransformer(HFTransformer):
-
     def training_step(self, batch, batch_idx):
         loss = self._step(batch, batch_idx, "train")
         return loss
@@ -22,12 +21,12 @@ class MultipleChoiceTransformer(HFTransformer):
         outputs = self.model(**batch)
         loss, logits = outputs[:2]
         preds = torch.argmax(logits, axis=1)
-        metric_dict = self._calculate_metrics(preds, batch['labels'], mode=mode)
+        metric_dict = self._calculate_metrics(preds, batch["labels"], mode=mode)
         self.log_dict(metric_dict, prog_bar=True, on_step=False, on_epoch=True)
-        self.log(f'{mode}_loss', loss, prog_bar=True, sync_dist=True)
+        self.log(f"{mode}_loss", loss, prog_bar=True, sync_dist=True)
         return loss
 
-    def configure_metrics(self):
+    def configure_metrics(self, stage: str):
         self.precision_metric = pl.metrics.Precision(num_classes=self.num_classes)
         self.recall_metric = pl.metrics.Recall(num_classes=self.num_classes)
         self.accuracy_metric = pl.metrics.Accuracy()
@@ -36,9 +35,9 @@ class MultipleChoiceTransformer(HFTransformer):
     def num_classes(self):
         return self.trainer.datamodule.num_classes
 
-    def _calculate_metrics(self, preds, labels, mode='val'):
+    def _calculate_metrics(self, preds, labels, mode="val"):
         # Not required by all models. Only required for classification
         p = self.precision_metric(preds, labels)
         r = self.recall_metric(preds, labels)
         a = self.accuracy_metric(preds, labels)
-        return {f'{mode}_precision': p, f'{mode}_recall': r, f'{mode}_acc': a}
+        return {f"{mode}_precision": p, f"{mode}_recall": r, f"{mode}_acc": a}
