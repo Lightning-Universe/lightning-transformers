1406738170c509154f9a07fcd2c32b6ccfbc049f
SeanNaren <sean@grid.ai>
2021-02-22 11:13:31 +0000

    Add finetuning callback, and changes required to hit higher model param counts

--- cli/train.py
+++ cli/train.py
@@ -1,9 +1,6 @@
-import time
 from typing import Any, Optional
 
-import torch
 from omegaconf import DictConfig, OmegaConf
-from pytorch_lightning import Callback
 from pytorch_lightning.utilities.distributed import rank_zero_info
 
 from lightning_transformers.core import TaskTransformer, TransformerDataModule
@@ -11,31 +8,6 @@ from lightning_transformers.core.config import TaskConfig, TrainerConfig, Transf
 from lightning_transformers.core.instantiator import HydraInstantiator, Instantiator
 from lightning_transformers.core.nlp.config import TokenizerConfig
 from lightning_transformers.core.utils import set_ignore_warnings
-
-
-class CUDACallback(Callback):
-
-    def on_train_epoch_start(self, trainer, pl_module):
-        # Reset the memory use counter
-        torch.cuda.reset_peak_memory_stats(trainer.root_gpu)
-        torch.cuda.synchronize(trainer.root_gpu)
-        self.start_time = time.time()
-
-    def on_train_epoch_end(self, trainer, pl_module, outputs):
-        torch.cuda.synchronize(trainer.root_gpu)
-        max_memory = torch.cuda.max_memory_allocated(trainer.root_gpu) / 2**20
-        epoch_time = time.time() - self.start_time
-
-        max_memory = torch.tensor(max_memory, dtype=torch.int, device=trainer.root_gpu)
-        epoch_time = torch.tensor(epoch_time, dtype=torch.int, device=trainer.root_gpu)
-
-        torch.distributed.all_reduce(max_memory, op=torch.distributed.ReduceOp.SUM)
-        torch.distributed.all_reduce(epoch_time, op=torch.distributed.ReduceOp.SUM)
-
-        world_size = torch.distributed.get_world_size()
-
-        print(f"Average Epoch time: {epoch_time.item() / float(world_size):.2f} seconds")
-        print(f"Average Peak memory {max_memory.item() / float(world_size):.2f}MiB")
 
 
 def run(
@@ -59,7 +31,10 @@ def run(
     data_module.setup("fit")
 
     model: TaskTransformer = instantiator.model(task, model_data_args=data_module.model_data_args)
-    trainer = instantiator.trainer(trainer, logger=logger, callbacks=[CUDACallback()])
+    trainer = instantiator.trainer(
+        trainer,
+        logger=logger,
+    )
 
     if do_train:
         trainer.fit(model, datamodule=data_module)
--- lightning_transformers/core/__init__.py
+++ lightning_transformers/core/__init__.py
@@ -1,2 +1,3 @@
 from lightning_transformers.core.data import TransformerDataModule  # noqa: F401
+from lightning_transformers.core.finetuning import TransformersBaseFinetuning  # noqa: F401
 from lightning_transformers.core.model import LitTransformer, TaskTransformer  # noqa: F401
