ce23f28fe4223c2ca96dddce3979a0b541e1dd62
SeanNaren <sean@grid.ai>
2021-03-04 12:45:45 +0000

    Add skipif condition, add sklearn to requirements.txt

--- tests/task/vision/test_igpt.py
+++ tests/task/vision/test_igpt.py
@@ -2,9 +2,11 @@ import sys
 from pathlib import Path
 
 import pytest
+from pytorch_lightning.utilities import _module_available
 
 
 @pytest.mark.skipif(sys.platform == "win32", reason="Currently Windows is not supported")
+@pytest.mark.skipif(not _module_available("torchvision"), reason="Requires torchvision to run")
 def test_smoke_end_to_end(hydra_runner, datadir):
     cache_dir: Path = datadir / "igpt/cifar/"
     cache_dir.mkdir(parents=True, exist_ok=True)
