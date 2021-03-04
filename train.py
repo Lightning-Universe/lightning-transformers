a5890f1d83a5a18c9083e5b34f6f75942df549c0
Sean Naren <sean.narenthiran@gmail.com>
2021-03-04 10:26:58 +0000

    Update train.py
    Co-authored-by: Carlos Mocholí <carlossmocholi@gmail.com>

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from omegaconf import DictConfig
 
 from lightning_transformers.cli.train import main
 
-
+"""The shell entry point `$ pl-transformers-train` is also available"""
 @hydra.main(config_path="./conf", config_name="config")
 def hydra_entry(cfg: DictConfig) -> None:
     main(cfg)
