907a2e671a631da0d0aad8673619fdde680270f7
Carlos Mocholi <carlossmocholi@gmail.com>
2021-03-09 15:57:48 +0100

    Fix TODO

--- lightning_transformers/cli/predict.py
+++ lightning_transformers/cli/predict.py
@@ -22,7 +22,8 @@ def run(
     model: HFTransformer = instantiator.model(task, model_data_args=model_data_args, tokenizer=tokenizer)
     if checkpoint_path:
         model.load_from_checkpoint(checkpoint_path)
-    return model.hf_predict(**x if isinstance(x, dict) else x, **predict_kwargs)
+    # TODO: fix dict input
+    return model.hf_predict(x, **predict_kwargs)
 
 
 def main(cfg: DictConfig) -> Any:
