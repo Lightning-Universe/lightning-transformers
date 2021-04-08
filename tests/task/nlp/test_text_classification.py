4a6c2e96b9d81a0f8a5ee8d30d35f778c6c376f9
Carlos Mocholi <carlossmocholi@gmail.com>
2021-04-08 16:55:48 +0200

    Remove + from dataset

--- tests/task/nlp/test_text_classification.py
+++ tests/task/nlp/test_text_classification.py
@@ -15,6 +15,13 @@ def test_smoke_train_e2e(script_runner):
     script_runner.hf_train(task='text_classification', dataset='emotion', model='prajjwal1/bert-tiny')
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="Currently Windows is not supported")
+def test_smoke_train_default_dataset(script_runner):
+    script_runner.hf_train(
+        task='text_classification', model='prajjwal1/bert-tiny', cmd_args=['dataset.cfg.dataset_name="emotion"']
+    )
+
+
 def test_smoke_predict_e2e(script_runner):
     y = script_runner.hf_predict(['+x="Lightning rocks!"'], task='text_classification', model='prajjwal1/bert-tiny')
     assert len(y) == 1
