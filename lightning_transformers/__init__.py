3fce2d6da1a4da671104cf15ace660e38581eb4f
jirka <jirka.borovec@seznam.cz>
2021-04-21 18:53:36 +0200

    refactor setup

--- lightning_transformers/__init__.py
+++ lightning_transformers/__init__.py
@@ -3,7 +3,7 @@ import os
 
 __ROOT_DIR__ = os.path.dirname(os.path.dirname(__file__))
 
-__version__ = "0.1.0rc1"
+__version__ = "0.1.0rc2"
 __author__ = "PyTorchLightning et al."
 __author_email__ = "name@pytorchlightning.ai"
 __license__ = 'Apache-2.0'
@@ -11,5 +11,15 @@ __copyright__ = f"Copyright (c) 2020-2020, {__author__}."
 __homepage__ = "https://github.com/PyTorchLightning/lightning-transformers"
 __docs__ = "PyTorch Lightning Transformers."
 __long_doc__ = """
-TODO
+Flexible interface for high performance research using SOTA Transformers leveraging PyTorch Lightning,
+ Transformers, and Hydra.
+
+Transformers are increasingly popular for SOTA deep learning, gaining traction in NLP with BeRT based architectures
+ more recently transcending into the world of Computer Vision and Audio Processing.
+
+However, training and fine-tuning transformers at scale is not trivial and can vary from domain to domain requiring
+ additional research effort, and significant engineering.
+
+Lightning Transformers gives researchers a way to train HuggingFace Transformer models with all the features
+ of PyTorch Lightning, while leveraging Hydra to provide composability of blocks and configs to focus on research.
 """
--- setup.py
+++ setup.py
@@ -3,52 +3,32 @@ import os
 
 from setuptools import find_packages, setup
 
-import lightning_transformers  # noqa: E402
-
-PATH_ROOT = os.path.dirname(__file__)
-
-
-def load_requirements(path_dir=PATH_ROOT, comment_char="#"):
-    with open(os.path.join(path_dir, "requirements.txt"), "r") as file:
-        lines = [ln.strip() for ln in file.readlines()]
-    reqs = [ln[:ln.index(comment_char)] if comment_char in ln else ln for ln in lines]
-    reqs = [ln for ln in reqs if ln]
-    return reqs
+import lightning_transformers as ltf
+from lightning_transformers import setup_tools
 
 
-def load_long_description():
-    url = os.path.join(
-        lightning_transformers.__homepage__,
-        "raw",
-        lightning_transformers.__version__,
-        "docs",
-    )
-    text = open("README.md", encoding="utf-8").read()
-    # replace relative repository path to absolute link to the release
-    text = text.replace("](docs", f"]({url}")
-    # SVG images are not readable on PyPI, so replace them  with PNG
-    text = text.replace(".svg", ".png")
-    return text
+_PATH_ROOT = os.path.dirname(__file__)
+long_description = setup_tools._load_readme_description(_PATH_ROOT, homepage=ltf.__homepage__, ver=ltf.__version__)
 
 
 setup(
     name="lightning-transformers",
-    version=lightning_transformers.__version__,
-    description=lightning_transformers.__docs__,
-    author=lightning_transformers.__author__,
-    author_email=lightning_transformers.__author_email__,
-    url=lightning_transformers.__homepage__,
+    version=ltf.__version__,
+    description=ltf.__docs__,
+    author=ltf.__author__,
+    author_email=ltf.__author_email__,
+    url=ltf.__homepage__,
     download_url="https://github.com/PyTorchLightning/lightning-transformers",
-    license=lightning_transformers.__license__,
+    license=ltf.__license__,
     packages=find_packages(exclude=["tests", "docs"]),
-    long_description=load_long_description(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     include_package_data=True,
     zip_safe=False,
     keywords=["deep learning", "pytorch", "AI"],
     python_requires=">=3.6",
     setup_requires=[],
-    install_requires=load_requirements(PATH_ROOT),
+    install_requires=setup_tools._load_requirements(_PATH_ROOT),
     entry_points={
         "console_scripts": [
             "pl-transformers-train=lightning_transformers.cli.train:hydra_entry",
@@ -57,7 +37,7 @@ setup(
     },
     project_urls={
         "Bug Tracker": "https://github.com/PyTorchLightning/lightning-transformers/issues",
-        # "Documentation": "TODO",
+        "Documentation": "https://lightning-transformers.readthedocs.io/en/stable/",
         "Source Code": "https://github.com/PyTorchLightning/lightning-transformers",
     },
     classifiers=[
@@ -77,8 +57,8 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
