b35f1547b8f117eebcba3af62ec70ff9f49858f6
Vision AI Labs 류민호 <ryumin93@sktbrain.com>
2021-05-22 16:57:11 +0900

    Implement Masked Language Modeling (#77)

---
+++ lightning_transformers/task/nlp/masked_language_modeling/config.py
@@ -0,0 +1,32 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from dataclasses import dataclass, field
+
+from lightning_transformers.core.nlp import HFTransformerDataConfig
+
+
+@dataclass
+class MaskedLanguageModelingDataConfig(HFTransformerDataConfig):
+    mlm_probability: float = field(
+        default=0.15,
+        metadata={"help": "Ratio of tokens to mask for masked language modeling loss"},
+    )
+    line_by_line: bool = field(
+        default=False,
+        metadata={"help": "Whether distinct lines of text in the dataset are to be handled as distinct sequences."},
+    )
+    wwm: bool = field(
+        default=False,
+        metadata={"help": "Whether or not use whole word masking"},
+    )
---
+++ lightning_transformers/task/nlp/masked_language_modeling/model.py
@@ -0,0 +1,54 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from lightning_transformers.core.nlp import HFTransformer
+
+
+class MaskedLanguageModelingTransformer(HFTransformer):
+    """
+    Defines ``LightningModule`` for the Masked Language Modeling Task.
+
+    Args:
+        *args: :class:`lightning_transformers.core.nlp.HFTransformer` arguments.
+        downstream_model_type: Downstream HuggingFace AutoModel to load. (default ``transformers.AutoModelForMaskedLM``)
+        **kwargs: :class:`lightning_transformers.core.nlp.HFTransformer` arguments.
+    """
+
+    def __init__(self, *args, downstream_model_type: str = 'transformers.AutoModelForMaskedLM', **kwargs) -> None:
+        super().__init__(downstream_model_type, *args, **kwargs)
+
+    def on_fit_start(self):
+        tokenizer_length = len(self.tokenizer)
+        self.model.resize_token_embeddings(tokenizer_length)
+
+    def _step(self, batch, batch_idx):
+        outputs = self.model(**batch)
+        loss = outputs[0]
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        loss = self._step(batch, batch_idx)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx, dataloader_idx=0):
+        loss = self._step(batch, batch_idx)
+        self.log("val_loss", loss, sync_dist=True)
+
+    def test_step(self, batch, batch_idx, dataloader_idx=0):
+        loss = self._step(batch, batch_idx)
+        self.log("test_loss", loss, sync_dist=True)
+
+    @property
+    def hf_pipeline_task(self) -> str:
+        return "fill-mask"
