2b1bd659916e0c0a552d0e03839aeec65ad4fb21
Minho Ryu <ryumin93@gmail.com>
2022-01-27 19:50:53 +0900

    import TransformerDataConfig from core.config
    It is natural to import TransformerDataConfig from core.config not core.data (even though TransformerDataConfig is imported in the script)

--- lightning_transformers/core/nlp/config.py
+++ lightning_transformers/core/nlp/config.py
@@ -2,8 +2,7 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional, Union
 
-from lightning_transformers.core.config import TaskConfig
-from lightning_transformers.core.data import TransformerDataConfig
+from lightning_transformers.core.config import TaskConfig, TransformerDataConfig
 
 
 @dataclass
