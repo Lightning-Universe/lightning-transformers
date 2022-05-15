631198c1cdb6d2db447e7b7e184e59232b0f26fb
SeanNaren <sean@grid.ai>
2022-05-15 13:17:44 +0100

    Fix tests

--- tests/core/test_model_step.py
+++ tests/core/test_model_step.py
@@ -5,58 +5,47 @@ from tests.core.boring_model import BoringDataModule, BoringTransformerModel
 
 
 class TestCallback(Callback):
-    def __init__(self, total_steps, input_wm_steps):
-        self.total_steps = total_steps
-        self.input_wm_steps = input_wm_steps
+    def __init__(self, expected_steps, expected_wm_steps):
+        self.expected_steps = expected_steps
+        self.expected_wm_steps = expected_wm_steps
 
     def on_fit_start(self, trainer, transformer_model: BoringTransformerModel) -> None:
-        assert transformer_model.num_training_steps == self.total_steps
-        if isinstance(self.input_wm_steps, int):
-            training_steps, warmup_steps = transformer_model.compute_warmup(-1, self.input_wm_steps)
-            assert training_steps == self.total_steps
-            assert warmup_steps == self.input_wm_steps
+        assert transformer_model.num_training_steps == self.expected_steps
+        if isinstance(self.expected_wm_steps, int):
+            training_steps, warmup_steps = transformer_model.compute_warmup(-1, self.expected_wm_steps)
+            assert training_steps == self.expected_steps
+            assert warmup_steps == self.expected_wm_steps
         else:
             # assume float value
-            training_steps, warmup_steps = transformer_model.compute_warmup(-1, self.input_wm_steps)
-            assert training_steps == self.total_steps
-            assert warmup_steps == (self.total_steps * self.input_wm_steps)
-        raise SystemExit
+            training_steps, warmup_steps = transformer_model.compute_warmup(-1, self.expected_wm_steps)
+            assert training_steps == self.expected_steps
+            assert warmup_steps == (self.expected_steps * self.expected_wm_steps)
 
 
 @pytest.mark.parametrize(
-    "max_epochs,num_processes,limit_train_batches,accumulate_grad_batches,input_wm_steps",
+    "max_epochs,devices,accumulate_grad_batches,expected_wm_steps",
     [
-        (5, 2, 5, 1, 5),
-        (5, 1, 1.0, 1, 5),
-        (5, 2, 1.0, 1, 5),
-        (5, 1, 5, 1, 5),
-        (6, 2, 6, 1, 5),
-        (5, 1, 5, 2, 5),
-        (6, 2, 6, 2, 5),
-        (6, 2, 6, 2, 0.5),
+        (5, 2, 1, 5),
+        (5, 1, 1, 5),
+        (5, 1, 2, 5),
+        (6, 2, 2, 5),
+        (6, 2, 2, 0.5),
     ],
 )
-def test_training_and_warmup_steps(
-    max_epochs, num_processes, limit_train_batches, accumulate_grad_batches, input_wm_steps
-):
+def test_training_and_warmup_steps(max_epochs, devices, accumulate_grad_batches, expected_wm_steps):
     model = BoringTransformerModel()
 
-    module = BoringDataModule()
-    num_steps = len(module.train_dataloader())
+    dm = BoringDataModule()
+    num_steps = len(dm.train_dataloader())
 
-    if isinstance(limit_train_batches, int) or limit_train_batches == 0.0:
-        num_steps = min(num_steps, int(limit_train_batches))
-    elif limit_train_batches != float("inf"):
-        num_steps = int(num_steps * limit_train_batches)
-    effective_batch_size = accumulate_grad_batches * num_processes
-    total_steps = (num_steps // effective_batch_size) * max_epochs
+    effective_batch_size = accumulate_grad_batches * devices
+    expected_steps = (num_steps // effective_batch_size) * max_epochs
 
     trainer = Trainer(
-        callbacks=TestCallback(total_steps=total_steps, input_wm_steps=input_wm_steps),
+        callbacks=TestCallback(expected_steps=expected_steps, expected_wm_steps=expected_wm_steps),
         max_epochs=max_epochs,
-        num_processes=num_processes,
-        limit_train_batches=limit_train_batches,
+        accelerator="cpu",
+        devices=devices,
         accumulate_grad_batches=accumulate_grad_batches,
     )
-    with pytest.raises(SystemExit):
-        trainer.fit(model, module)
+    trainer.fit(model, dm)
