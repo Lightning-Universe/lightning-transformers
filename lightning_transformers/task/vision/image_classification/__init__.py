9f25baa5e902ad50e91a524e06ce9c1080adc1fa
Tanmoy <tanmoyf2@gmail.com>
2022-05-28 04:37:46 +0530

    ViT Image Classification Support (#252)

---
+++ lightning_transformers/task/vision/image_classification/__init__.py
@@ -0,0 +1,3 @@
+from lightning_transformers.task.vision.image_classification.config import ImageClassificationDataConfig  # noqa: F401
+from lightning_transformers.task.vision.image_classification.data import ImageClassificationDataModule  # noqa: F401
+from lightning_transformers.task.vision.image_classification.model import ImageClassificationTransformer  # noqa: F401
---
+++ lightning_transformers/task/vision/image_classification/config.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+from lightning_transformers.core.config import TransformerDataConfig
+
+
+@dataclass
+class ImageClassificationDataConfig(TransformerDataConfig):
+    ...
