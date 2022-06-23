dc95281acbbfd6afc74d6a9258cf34d3946d8aa7
SeanNaren <sean@grid.ai>
2022-06-23 11:18:41 +0100

    Remove configs

--- lightning_transformers/task/nlp/token_classification/__init__.py
+++ lightning_transformers/task/nlp/token_classification/__init__.py
@@ -1,3 +1,2 @@
-from lightning_transformers.task.nlp.token_classification.config import TokenClassificationDataConfig  # noqa: F401
 from lightning_transformers.task.nlp.token_classification.data import TokenClassificationDataModule  # noqa: F401
 from lightning_transformers.task.nlp.token_classification.model import TokenClassificationTransformer  # noqa: F401
