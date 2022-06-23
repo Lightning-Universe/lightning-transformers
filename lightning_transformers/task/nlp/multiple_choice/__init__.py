62a019586ed622c1a20028238a5869516f15aa59
SeanNaren <sean@grid.ai>
2022-06-23 11:16:03 +0100

    Remove configs

--- lightning_transformers/task/nlp/multiple_choice/__init__.py
+++ lightning_transformers/task/nlp/multiple_choice/__init__.py
@@ -1,4 +1,3 @@
-from lightning_transformers.task.nlp.multiple_choice.config import MultipleChoiceDataConfig  # noqa: F401
 from lightning_transformers.task.nlp.multiple_choice.data import MultipleChoiceDataModule  # noqa: F401
 from lightning_transformers.task.nlp.multiple_choice.datasets import (  # noqa: F401
     RaceMultipleChoiceDataModule,
