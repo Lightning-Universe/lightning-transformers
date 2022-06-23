5409e07c2cd2ba7d2b999ceec7c87fae3f3f4893
SeanNaren <sean@grid.ai>
2022-06-23 11:44:26 +0100

    Remove configs

--- lightning_transformers/task/vision/image_classification/__init__.py
+++ lightning_transformers/task/vision/image_classification/__init__.py
@@ -1,3 +1,2 @@
-from lightning_transformers.task.vision.image_classification.config import ImageClassificationDataConfig  # noqa: F401
 from lightning_transformers.task.vision.image_classification.data import ImageClassificationDataModule  # noqa: F401
 from lightning_transformers.task.vision.image_classification.model import ImageClassificationTransformer  # noqa: F401
