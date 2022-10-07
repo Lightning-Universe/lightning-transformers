1ff0503d578b9cbbd653d9b8a206b27e62edfc68
Jirka <jirka.borovec@seznam.cz>
2022-10-07 23:46:12 +0200

    use lit utils

--- lightning_transformers/utilities/imports.py
+++ lightning_transformers/utilities/imports.py
@@ -13,10 +13,9 @@
 # limitations under the License.
 import operator
 
-from pytorch_lightning.utilities import _module_available
-from pytorch_lightning.utilities.imports import _compare_version
+from lightning_utilities.core.imports import module_available, compare_version
 
-_BOLTS_AVAILABLE = _module_available("pl_bolts") and _compare_version("pl_bolts", operator.ge, "0.4.0")
-_BOLTS_GREATER_EQUAL_0_5_0 = _module_available("pl_bolts") and _compare_version("pl_bolts", operator.ge, "0.5.0")
-_WANDB_AVAILABLE = _module_available("wandb")
-_ACCELERATE_AVAILABLE = _module_available("accelerate")
+_BOLTS_AVAILABLE = module_available("pl_bolts") and compare_version("pl_bolts", operator.ge, "0.4.0")
+_BOLTS_GREATER_EQUAL_0_5_0 = module_available("pl_bolts") and compare_version("pl_bolts", operator.ge, "0.5.0")
+_WANDB_AVAILABLE = module_available("wandb")
+_ACCELERATE_AVAILABLE = module_available("accelerate")
