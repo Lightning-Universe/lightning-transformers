b6142fe28cf8528c30b6c7f60bd8e2f91282d56f
Rohit Gupta <rohitgr1998@gmail.com>
2022-11-03 23:02:56 +0530

    release 0.2.4 (#301)
    * version 0.2.4
    * chlog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,14 +4,8 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
-
-### Added
+## [0.2.4] - 2022-11-03
 
 ### Changed
 
-### Fixed
-
-### Removed
-
-### Deprecated
+- Added support for Lightning v1.8.0 ([#297](https://github.com/Lightning-AI/lightning-transformers/pull/297))
--- lightning_transformers/__init__.py
+++ lightning_transformers/__init__.py
@@ -3,14 +3,14 @@ import os
 
 __ROOT_DIR__ = os.path.dirname(os.path.dirname(__file__))
 
-__version__ = "0.2.3"
-__author__ = "PyTorchLightning et al."
-__author_email__ = "name@pytorchlightning.ai"
+__version__ = "0.2.4"
+__author__ = "Lightning AI et al."
+__author_email__ = "pytorch@lightning.ai"
 __license__ = "Apache-2.0"
 __copyright__ = f"Copyright (c) 2020-2020, {__author__}."
-__homepage__ = "https://github.com/PyTorchLightning/lightning-transformers"
-__docs__ = "PyTorch Lightning Transformers."
+__homepage__ = "https://github.com/Lightning-AI/lightning-transformers"
+__docs__ = "Lightning Transformers."
 __long_doc__ = """
 Lightning Transformers provides `LightningModules`, `LightningDataModules` and `Strategies` to use
-HuggingFace Transformers with the PyTorch Lightning Trainer.
+HuggingFace Transformers with the Lightning Trainer.
 """
